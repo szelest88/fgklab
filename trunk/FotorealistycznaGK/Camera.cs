32d6eb00f4fc7116ff0ba4d5033cc88574bdc66a
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-03-28 11:41:01 +0000

    dodałęm klasę kamery

---
+++ trunk/FotorealistycznaGK/Camera.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FotorealistycznaGK
+{
+    public class Camera
+    {
+        private Vector position;
+
+        public Vector Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        private Vector target;
+
+        public Vector Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+
+        private Vector up;
+
+        public Vector Up
+        {
+            get { return up; }
+            set { up = value; }
+        }
+
+        private float nearPlane;
+
+        public float NearPlane
+        {
+            get { return nearPlane; }
+            set { nearPlane = value; }
+        }
+
+        private float farPlane;
+
+        public float FarPlane
+        {
+            get { return farPlane; }
+            set { farPlane = value; }
+        }
+
+        private float fov;
+
+        public float Fov
+        {
+            get { return fov; }
+            set { fov = value; }
+        }
+
+        public Camera()
+        {
+            this.position = new Vector(0,0,0);
+            this.target = new Vector(0,0,1);
+            this.nearPlane = 1;
+            this.farPlane = 1000;
+            this.up = new Vector(0,1,0);
+        }
+
+        public Camera(Vector position, Vector target)
+        {
+            this.position = position;
+            this.target = target;
+            this.nearPlane = 1;
+            this.farPlane = 1000;
+            this.up = new Vector(0, 1, 0);
+        }
+
+
+
+
+
+    }
+}
