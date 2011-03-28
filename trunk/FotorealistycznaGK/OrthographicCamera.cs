fe60d194ac5d5792a8ec402e4cebcd41821a28d0
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-03-28 11:51:06 +0000

    Tak dla picu dodałem.

---
+++ trunk/FotorealistycznaGK/OrthographicCamera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FotorealistycznaGK
+{
+    //tak na oko... Chociaż nawet nie wiem, czy to ma sens. Tzn. może klasa
+    //Camera powinna być jakoś ogólniejsza - jaki jest sens dziedziczenie fov
+    //po kamerze przez kamerę Orto?
+    class OrthographicCamera: Camera
+    {
+    }
+}
