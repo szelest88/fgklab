c9f57337d1ff393f0ec4773f08a98c2c36e6644b
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-04-05 10:27:10 +0000

    Let there be Light.cs.
    And there was Light.cs.

--- trunk/FotorealistycznaGK/Light.cs
+++ trunk/FotorealistycznaGK/Light.cs
@@ -7,5 +7,38 @@ namespace FotorealistycznaGK
 {
     abstract class Light
     {
+        Intensity color; // kolor swiatla
+        Vector position; // pozycja swiatla w ukladzie xyz
+
+        #region Properties
+
+        public Intensity Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public Vector Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        #endregion Properties
+
+        public Light()
+        {
+            this.Color = new Intensity(1, 1, 1);
+            this.Position = new Vector(0, 0, 0);
+        }
+
+        // nie wiem czy pozycji nie trza jakos powiazac z kamera - ale tak glosno mysle :>
+
+        public Light(Intensity I, Vector v)
+        {
+            this.Color = I;
+            this.Position = v;
+        }
+
     }
 }
