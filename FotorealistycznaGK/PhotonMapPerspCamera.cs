a76e4356175b8c3909d9b0b4f9ff3e076159805c
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-05-30 12:32:27 +0000

    Dodałem, żeby zacząć ten szajs.

---
+++ FotorealistycznaGK/PhotonMapPerspCamera.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FotorealistycznaGK
+{
+    public class PhotonMapPerspCamera: Camera
+    {
+
+    }
+}
