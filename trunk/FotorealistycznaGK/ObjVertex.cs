a1e8041283f206ccc98076d81a76f5f8af5a9b80
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-06-07 02:24:20 +0000

    wczesna wersja loadera

---
+++ trunk/FotorealistycznaGK/ObjVertex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FotorealistycznaGK
+{
+    public class ObjVertex
+    {
+        public double x, y, z;
+        public ObjVertex(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public override string ToString()
+        {
+            return "Wierzchołek: x = " + x + ", y = " + y + ",z = " + z;
+        }
+    }
+}
