ac185d4d560ba33022e407f63b8da6de86cd88d6
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-06-07 02:24:20 +0000

    wczesna wersja loadera

---
+++ FotorealistycznaGK/Loader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Globalization;
+
+namespace FotorealistycznaGK
+{
+    class Loader{
+    
+         public static void read()
+        {
+            /*przyjąłem format pliku:
+v 0.000000 2.000000 2.000000
+v 0.000000 0.000000 2.000000
+v 2.000000 0.000000 2.000000
+v 2.000000 2.000000 2.000000
+v 0.000000 2.000000 0.000000
+v 0.000000 0.000000 0.000000
+v 2.000000 0.000000 0.000000
+v 2.000000 2.000000 0.000000
+f 1 2 3
+f 2 3 4
+f 8 7 6
+f 7 6 5
+f 4 3 7
+f 3 7 8
+f 5 1 4
+f 1 4 8
+f 5 6 2
+f 6 2 1
+f 2 6 7
+f 6 7 3
+
+    ...czyli chwilowo jeno werteksy i face'y, i każdy face jest
+    trójkątem.
+             * */
+
+            //myk do uznania kropki za separator dziesiętny
+            #region myk
+            string currentCulture = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
+            CultureInfo ci = new CultureInfo(currentCulture);
+            //Ustawiamy nowy format
+            ci.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+            #endregion myk
+
+            StreamReader sr = new StreamReader(@"C:\moj_plik.txt");
+            string s;
+            // stringi opisujące współrzędne verteksów:
+            List<string> verticesStrings = new List<string>();
+            while ((s = sr.ReadLine()) != null)
+            {
+                if (s != "" && s[0] == 'v' && s[1] != 'n')
+                    verticesStrings.Add(s);
+            }
+
+            // współrzędne verteksów:
+            List<ObjVertex> verticesNums = new List<ObjVertex>();
+            foreach (string s2 in verticesStrings)
+            {
+                string[] temp = s2.Split();
+
+                ObjVertex ov = new ObjVertex(
+                    double.Parse(temp[1]),
+                    double.Parse(temp[2]),
+                    double.Parse(temp[3]));
+                verticesNums.Add(ov);
+            }
+            sr.Close();
+
+            sr = new StreamReader(@"C:\moj_plik.txt");
+
+            // stringi opisujące indeksy werteksów w face'ach
+            List<string> facesStrings = new List<string>();
+            while ((s = sr.ReadLine()) != null)
+            {
+                if (s != "" && s[0] == 'f' && s[1] != 'n')
+                    facesStrings.Add(s);
+            }
+
+            //zamiana ich na liczby i wczytanie dla każdego face'a
+            //odpowiednich współrzędnych
+            foreach (string s2 in facesStrings)
+            {
+                System.Console.WriteLine("FACE:");
+                string[] podzielony = s2.Split();
+                int index1 = int.Parse(podzielony[1]) - 1;
+                int index2 = int.Parse(podzielony[2]) - 1;
+                int index3 = int.Parse(podzielony[3]) - 1;
+                System.Console.WriteLine(verticesNums[index1]);
+                System.Console.WriteLine(verticesNums[index2]);
+                System.Console.WriteLine(verticesNums[index3]);
+            }
+        }
+    }
+}
