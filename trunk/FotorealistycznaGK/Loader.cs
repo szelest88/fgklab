7829452e9d713a0ee7d04aad7e6ca7e368dd029c
szelest88 <szelest88@7d8509ab-f483-da5c-ede9-d609fc7fb254>
2011-06-13 22:54:29 +0000

    mocno nieskoordynowany loader

--- trunk/FotorealistycznaGK/Loader.cs
+++ trunk/FotorealistycznaGK/Loader.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Text.RegularExpressions;
 using System.IO;
 using System.Globalization;
 
 namespace FotorealistycznaGK
 {
     class Loader{
-    
-         public static void read()
+
+        public List<Primitive> listaZajebista;
+
+        public void read(float scale, float x, float y, float z)
         {
+
             /*przyjąłem format pliku:
 v 0.000000 2.000000 2.000000
 v 0.000000 0.000000 2.000000
@@ -39,15 +42,17 @@ f 6 7 3
              * */
 
             //myk do uznania kropki za separator dziesiętny
+            listaZajebista = new List<Primitive>();
+
             #region myk
             string currentCulture = System.Threading.Thread.CurrentThread.CurrentCulture.Name;
             CultureInfo ci = new CultureInfo(currentCulture);
-            //Ustawiamy nowy format
+            //Ustawiamy nowy format 
             ci.NumberFormat.NumberDecimalSeparator = ".";
             System.Threading.Thread.CurrentThread.CurrentCulture = ci;
             #endregion myk
 
-            StreamReader sr = new StreamReader(@"C:\moj_plik.txt");
+            StreamReader sr = new StreamReader(@"C:\teapot.obj");
             string s;
             // stringi opisujące współrzędne verteksów:
             List<string> verticesStrings = new List<string>();
@@ -61,7 +66,9 @@ f 6 7 3
             List<ObjVertex> verticesNums = new List<ObjVertex>();
             foreach (string s2 in verticesStrings)
             {
-                string[] temp = s2.Split();
+                char[] splitter = { ' ' };
+                string[] temp = s2.Split(splitter, StringSplitOptions.RemoveEmptyEntries);//spearatorem jest x spacji - i co kurwa?
+
 
                 ObjVertex ov = new ObjVertex(
                     double.Parse(temp[1]),
@@ -71,7 +78,7 @@ f 6 7 3
             }
             sr.Close();
 
-            sr = new StreamReader(@"C:\moj_plik.txt");
+            sr = new StreamReader(@"C:\teapot.obj");
 
             // stringi opisujące indeksy werteksów w face'ach
             List<string> facesStrings = new List<string>();
@@ -86,13 +93,23 @@ f 6 7 3
             foreach (string s2 in facesStrings)
             {
                 System.Console.WriteLine("FACE:");
-                string[] podzielony = s2.Split();
+                string[] podzielony = Regex.Split(s2, " +");
+                //  string[] podzielony = s2.Split({' '},StringSplitOptions.RemoveEmptyEntries);
+
                 int index1 = int.Parse(podzielony[1]) - 1;
                 int index2 = int.Parse(podzielony[2]) - 1;
                 int index3 = int.Parse(podzielony[3]) - 1;
                 System.Console.WriteLine(verticesNums[index1]);
                 System.Console.WriteLine(verticesNums[index2]);
                 System.Console.WriteLine(verticesNums[index3]);
+                listaZajebista.Add(
+                    new Triangle(
+                    new Vector(x + scale * (float)verticesNums[index1].x, y + scale * (float)verticesNums[index1].y, z + scale * (float)verticesNums[index1].z),
+                    new Vector(x + scale * (float)verticesNums[index2].x, y + scale * (float)verticesNums[index2].y, z + scale * (float)verticesNums[index2].z),
+                    new Vector(x + scale * (float)verticesNums[index3].x, y + scale * (float)verticesNums[index3].y, z + scale * (float)verticesNums[index3].z),
+                    new Intensity(1, 0, 0),
+                    new Material())
+                    );
             }
         }
     }
